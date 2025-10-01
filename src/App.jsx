dd54ebf0ae2049517735f7be9215baa150ac0690
gdrmvrm <gdrmvrm@gmail.com>
2025-10-01 11:32:28 +0100

    Set up state value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import list from './data';
+
 const App = () => {
+  const [questions, setQuestions] = useState(list);
+
   return <h2>Accordion Starter</h2>;
 };
 export default App;
